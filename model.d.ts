e22ffffddc355afcd90620fd3bbf973675ecd7eb
Matthew5 <jnwu@thoughtworks.com>
2019-02-06 23:29:31 +0800

    Add basic code

---
+++ model.d.ts
@@ -0,0 +1,16 @@
+interface Action<Payload> {
+    type: string
+    payload?: Payload
+    error?: boolean
+  }
+  
+  interface ModelPromise {
+    resolve: Function
+    reject: Function
+    then: Function
+  }
+  
+  declare interface ModelAction0 extends Action<any>, ModelPromise {}
+  
+  declare interface ModelAction<Payload> extends Action<Payload>, ModelPromise {}
+  
